a7a895150726edb644b0c6ed198ce5be5cfddc9f
aryan-8947 <smartaryanagrawal987@gmail.com>
2022-01-05 01:12:05 +0530

    final submission

--- Routes/updatepassword.js
+++ Routes/updatepassword.js
@@ -9,15 +9,15 @@ require('../config/db.js')
 router.put('/',authenticate,async (req,res)=>{
     
     try{
-        
+        const {password,newpassword}=req.body;
             
         const userLogin= await User.findOne({_id:req.rootuser._id})
-        const  isMatch=await bcrypt.compare(req.body.password,userLogin.password); 
+        const  isMatch=await bcrypt.compare(password,userLogin.password); 
         if(isMatch){
             const update= await User.updateOne({_id:userLogin._id},{
                 
               $set:{
-                 password:await bcrypt.hash(req.body.newpassword,12)
+                 password:await bcrypt.hash(newpassword,12)
               }
             })
             if(update) res.status(201).json({message:'done update'});
--- server.js
+++ server.js
@@ -44,8 +44,76 @@ app.use('/api/validateemail',validateemail);
 app.use('/api/googlelogin',googlelogin);
 app.use('/api/resetpassword',resetpassword);
 
+
+const message=[
+    "welcome to  user control api",
+    "routes                                 req                                       type",
+
+    "/api/signup                    email,name,dob,mobilenumber,password              post",
+    "/api/login                     email/mobilenumber,passowrd                       post",
+    "/api/logout                                                                      get ",
+    "/api/getdata                                                                     get",
+    "/api/updatedata                email,name,dob,mobilenumber                       post",
+    "/api/resetpassword             email,otp,password                                post",
+    "/api/updatepassword            password,newpassword                              put",
+    "/api/googlelogin               tokenid                                           post",
+    "/api/validateemail             otp                                               post",
+    "/api/mailotp                   email                                             post",
+    "/api/debitcardedit             _id,types,cardnumber,expirydate                   patch",
+    "/api/debitcardadd              types,cardnumber,expirydate                       post",
+    "/api/debitcarddelete           _id                                               patch",
+    "/api/addressdelete             _id                                               patch",
+    "/api/addressedit               _id,types,address                                 patch",
+    "/api/addressadd                types,address                                     post",
+        ` `,
+       ` `,
+    "schema example",
+     ` `,
+    `_id:ObjectId("61d49c63919fded5418895bb")`,
+    ` email:"hi@gamil.com"`,
+    `name:"abc"`,
+    `dob:"2012-17-03"`,
+    `mobilenumber:"1234567890"`,
+    `password:"$2a$12$WX/Hd7jUB2Tp.5CmR3zO3egrAHDgcVAtR1EIryHW9SfpC2XYO6pPy"`,
+    `validatecheck:false`,
+    `addresses:Array`,
+    `   0:Object`,
+    `       types:"home"`,
+    `       address:" at home"`,
+    `       _id:ObjectId("61d49ebfe3ff4312d3769ccc")`,
+    `cards:Array`,
+   `    0:Object`,
+    `       types:"debit"`,
+    `       cardnumber:"1234567812341234"`,
+    `       expirydate:"12/2021"`,
+    `       _id:ObjectId("61d49d8ae3c8cbf59e087ce1")`,
+    `tokens:Array`,
+    `   0:Object`,
+    `       token:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWQ0OWM2MzkxOWZkZWQ1N..."`,
+    `       _id:ObjectId("61d49f04e3ff4312d3769cd1")`,
+    `__v:3`,
+    `otp:Object`,
+    `   expirydate:2022-01-04T19:56:05.653+00:00`,
+    `   otpno:638863`
+
+
+   
+        
+
+
+
+
+
+
+
+];
+
+
+
 app.get('/',(req,res)=>{
-    res.send('login and signup');
+    res.json(message);
+    
+    
 });
 app.use((req,res) => {
     res.status(404).json({message : "Page Not Found"})
